9ca4bfcfa6e3ddd21d777826c8a8cfa56a32fa91
Anirban Azad <azadarannya@gmail.com>
2020-10-07 09:11:07 +0600

    App.js fixed with a new extended screen

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainar} from '@react-navigation/native';
+import {CreateStackNavigator} from '@react-navigation/stack';
+
+imp
 
 export default function App() {
   return (
